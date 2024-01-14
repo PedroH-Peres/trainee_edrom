44e73b3578c22209cf74bd6157a39b6f5762a8be
PedroH.Peres <pedrohperescode@gmail.com>
2024-01-14 12:28:56 -0300

    Visao1

--- Visao/tarefa1/capacitacao1.py
+++ Visao/tarefa1/capacitacao1.py
@@ -1,3 +1,20 @@
 # Pedro Henrique Bohling Peres
 # Tarefa 1 - Visão EDROM
 
+import os
+
+if not os.path.exists("completes"):
+    os.mkdir("completes")
+
+for arq in os.listdir("labels"):
+    cur = open(f"labels/{str(arq)}", "r")
+
+    txt = cur.read()
+    txt = txt.replace("0", "1", 1)
+    cur.close()
+
+    cur = open(f"labels/{str(arq)}", "w")
+    cur.write(txt)
+    cur.close()
+    os.rename(f"labels/{str(arq)}", f"completes/computer{arq[len(arq)-5]}")
+
